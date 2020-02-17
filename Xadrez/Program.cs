796a9d0a1aee3fa37e19fe187ec6170f30aeb998
Vinicius Andrade <vinny_fa2000@hotmail.com>
2020-02-17 09:56:29 -0300

    Começando a colocar peças no tabuleiro.

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
-namespace Xadrez
-{
+namespace Program{
     class Program
     {
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+           
+            tab.colorcarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
+            tab.colorcarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colorcarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));            
 
             Tela.imprimirTabuleiro(tab);
-
 
             Console.ReadLine();
         }
