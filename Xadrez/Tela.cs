983e7ca85f8346d7bb732d2c71e97f14d5d4c31b
Vinicius Andrade <vinny_fa2000@hotmail.com>
2020-02-26 15:15:46 -0300

    Acrescentando as demais pecas.

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -65,7 +65,7 @@ namespace Program
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("  A B C D E F G H");
+            Console.WriteLine("  a b c d e f g h");
         }
 
         public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis)
